d1105d41553a37d33d075ffac7fc901a30e6bf7d
Prof. Dr. Hasan Ali Khattak <hasan.alikhattak@seecs.edu.pk>
2024-03-19 17:00:03 +0400

    week09

---
+++ www/lab10/action-get.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?> <br>
+and password: <?php echo $_GET["password"]; ?> <br>
+</body>
+</html>
---
+++ www/lab10/action.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?> <br>
+and password: <?php echo $_POST["password"]; ?> <br>
+</body>
+</html>
