205fa8a58a17bea950043db028a341ceb3bf1694
Dr. Hasan Ali Khattak <hasan.alikhattak@gmail.com>
2024-04-08 13:10:29 +0400

    added week 11 and week 12 tutorials

---
+++ bin/heidisql/Backups/query-tab-2024-04-08_09-42-17-216.sql
@@ -0,0 +1,7 @@
+-- Insert sample data
+INSERT INTO hero (name, powers, image_link) VALUES
+('Superman', 'Flight, Super strength, Heat vision', 'https://superherodb.com/pictures2/portraits/10/100/791.jpg'),
+('Spider-Man', 'Wall-crawling, Superhuman strength, Spidey sense', 'https://superherodb.com/pictures2/portraits/10/100/133.jpg'),
+('Wonder Woman', 'Super strength, Flight, Invulnerability', 'https://superherodb.com/pictures2/portraits/10/100/807.jpg'),
+('Batman', 'Intelligence, Martial arts, Wealth', 'https://superherodb.com/pictures2/portraits/10/100/639.jpg'),
+('Captain America', 'Peak human strength, Shield proficiency', 'https://superherodb.com/pictures2/portraits/10/100/274.jpg');
